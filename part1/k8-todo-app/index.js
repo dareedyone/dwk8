29f08e270697ee6d97eb7fb150d0c58854089084
Ridwan Ijadunola <dareedyone@gmail.com>
2020-10-27 23:05:50 +0100

    Exercise 1.05: Project v0.3

--- part1/k8-todo-app/index.js
+++ part1/k8-todo-app/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 
-const port = 3001;
-
+const port = process.env.PORT ||     3001;
+app.get("/", (req, res) => {
+res.send("<h1>here we go !</h1>")
+})
 app.listen(port, ()=> {
     console.log(`server listeninng on port ${port}`);
 })
