056257a34e4ea4f57a9c940e23c2505cd14b5f08
Ridwan Ijadunola <dareedyone@gmail.com>
2020-10-28 21:41:12 +0100

    Exercise 1.07: External access with Ingress

--- part1/k8-todo-app/index.js
+++ part1/k8-todo-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 
-const port = process.env.PORT ||     3001;
+const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
 res.send("<h1>here we go !</h1>")
 })
--- part1/random-string-generator/index.js
+++ part1/random-string-generator/index.js
@@ -1,12 +1,23 @@
-const randomString = () => Math.random().toString(36).slice(2);
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
+const randomString = () => Math.random().toString(36).slice(2);
 const randomStringGen = () => {
 const timestamp = new Date().toISOString();
 const str = `${timestamp}: ${randomString()}-${randomString()}-${randomString()}-${randomString()}`
 console.log(str);
+return str;
 };
 
 setInterval(() => {
     randomStringGen()
 }, 5000);
 
+
+app.get("/", (req, res) => {
+res.send(`<h4>${randomStringGen()}</h4>`)
+})
+app.listen(port, ()=> {
+    console.log(`server listeninng on port ${port}`);
+})
